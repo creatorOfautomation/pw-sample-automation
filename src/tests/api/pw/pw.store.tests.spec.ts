078d1c01b3c0aa811eac69fa0c2557086a55480b
Oleksandr V <zdp94@ukr.net>
2024-09-04 17:38:15 +0300

    Add Store endpoint tests

---
+++ src/tests/api/pw/pw.store.tests.spec.ts
@@ -0,0 +1,18 @@
+import {test} from "@playwright/test";
+import PWApiClient from "../../../api/controllers/pw/pw-api-client";
+import {definitions} from "../../../../.temp/types";
+
+const client = PWApiClient.unauthorized();
+
+test.describe('PW - STORE API test cases', () => {
+    test('PW - Verify Order can be placed', async () => {
+        const body: Omit<definitions['Order'], 'id'> = {
+            petId: 1,
+            quantity: 1,
+            shipDate: new Date().toISOString(),
+            status: 'placed'
+        };
+
+        await client.store.placeOrder(body);
+    })
+})
